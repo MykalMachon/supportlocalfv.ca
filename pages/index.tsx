19ef3118bbf7413c2fbbf1bb42b8620db09f4df5
Mykal Machon <mykalmachon@gmail.com>
2020-03-21 18:52:19 -0700

    Fix: some weird now.json bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { GetStaticProps } from 'next';
+
 import fetch from 'isomorphic-unfetch';
 import { generateFetchSettings } from '../src/util';
 import Layout from '../src/components/layout/Layout';
@@ -6,14 +8,21 @@ const HomePage = ({ data: { businesses } }) => {
   return (
     <Layout title="Home">
       <h2>This is my home page</h2>
+      <ul className="businesses">
+        {businesses.map((biz) => {
+          return <li key={biz.id}>{biz.businessName}</li>;
+        })}
+      </ul>
     </Layout>
   );
 };
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async (context) => {
+  console.log(process.env);
+  console.log(process.env.SLFV_TOKEN);
   const res = await fetch(
     'https://v2-api.sheety.co/mykal/supportlocalfv/businesses',
-    generateFetchSettings(process.env.SLFV_TOKEN)
+    { ...generateFetchSettings(process.env.SLFV_TOKEN) }
   );
   const data = await res.json();
   return {
