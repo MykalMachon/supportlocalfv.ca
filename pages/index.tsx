7b2f3c9a9f6998f61090079121eaa262c038af59
Mykal Machon <mykalmachon@gmail.com>
2020-03-22 16:12:02 -0700

    Layout Adjustments for Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,21 @@ import { GetStaticProps } from 'next';
 import fetch from 'isomorphic-unfetch';
 import { generateFetchSettings } from '../src/util';
 import Layout from '../src/components/layout/Layout';
+import BusinessCard from '../src/components/BusinessCard';
+import BusinessCardList from '../src/components/BusinessCardList';
 
 const HomePage = ({ data: { businesses } }) => {
   return (
     <Layout title="Home">
-      <h2>This is my home page</h2>
-      <ul className="businesses">
-        {businesses.map((biz) => {
-          return <li key={biz.id}>{biz.businessName}</li>;
-        })}
-      </ul>
+      <section className="container">
+        <h2>This is my home page</h2>
+        <BusinessCardList bizlist={businesses} />
+      </section>
     </Layout>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  console.log(process.env);
-  console.log(process.env.SLFV_TOKEN);
   const res = await fetch(
     'https://v2-api.sheety.co/mykal/supportlocalfv/businesses',
     { ...generateFetchSettings(process.env.SLFV_TOKEN) }
