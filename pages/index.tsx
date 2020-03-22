0a6cf46a4ce8c860e318ae22a2fac810f434dc29
Mykal Machon <mykalmachon@gmail.com>
2020-03-21 18:14:16 -0700

    Removing Static Props Generation for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,17 +10,17 @@ const HomePage = ({ data: { businesses } }) => {
   );
 };
 
-export const getStaticProps = async () => {
-  const res = await fetch(
-    'https://v2-api.sheety.co/mykal/supportlocalfv/businesses',
-    generateFetchSettings(process.env.SLFV_TOKEN)
-  );
-  const data = await res.json();
-  return {
-    props: {
-      data,
-    },
-  };
-};
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     'https://v2-api.sheety.co/mykal/supportlocalfv/businesses',
+//     generateFetchSettings(process.env.SLFV_TOKEN)
+//   );
+//   const data = await res.json();
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
 
 export default HomePage;
