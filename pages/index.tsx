b1729d2585bc8934b9cafb2d45301065963756d4
Mykal Machon <mykalmachon@gmail.com>
2020-03-22 17:55:24 -0700

    Added Secondary Site Styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,33 @@ import { GetStaticProps } from 'next';
 import fetch from 'isomorphic-unfetch';
 import { generateFetchSettings } from '../src/util';
 import Layout from '../src/components/layout/Layout';
-import BusinessCard from '../src/components/BusinessCard';
-import BusinessCardList from '../src/components/BusinessCardList';
+import BusinessCardList from '../src/components/business/BusinessCardList';
+import Link from 'next/link';
 
 const HomePage = ({ data: { businesses } }) => {
   return (
     <Layout title="Home">
       <section className="container">
-        <h2>This is my home page</h2>
+        <article className="intro_container">
+          <div className="intro_box">
+            <h2>Lets Support Local in The Fraser Valley</h2>
+            <p>
+              With local businesses, shops, restaurants, and cafes around the
+              Fraser Valley temporarily shut down due to COVID-19, many folks
+              are struggling. This site is meant to be a resource for residents
+              of the Fraser Valley to find local businesses and support them
+              however they can.
+            </p>
+            <p>
+              This information is crowd-sourced, so please verify the accuracy
+              independently. If you see a mistake, please{' '}
+              <Link href="/contactUs">
+                <a>contact us</a>
+              </Link>
+              .
+            </p>
+          </div>
+        </article>
         <BusinessCardList bizlist={businesses} />
       </section>
     </Layout>
