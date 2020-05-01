5246362b10e8bde06c1c0b761f0ab8554d675ce6
Mykal Machon <mykalmachon@gmail.com>
2020-05-01 00:26:54 -0700

    Added Links to Footer
    Added the relevant links to the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,23 +8,55 @@ const Footer: FC<FooterProps> = ({}) => {
         <div className='footer_content-column'>
           <h4>On This Site</h4>
           <ul>
-            <li>About SLFV</li>
-            <li>Submit your business</li>
-            <li>Contact us</li>
+            <li>
+              <Link href='/about'>
+                <a>About SLFV</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/submit'>
+                <a>Submit your business</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/contact'>
+                <a>Contact us</a>
+              </Link>
+            </li>
           </ul>
         </div>
         <div className='footer_content-column'>
           <h4>Related Resources</h4>
           <ul>
-            <li>Abby Eats Local</li>
-            <li>Okay for Oklahoma</li>
-            <li>Stats Canada</li>
+            <li>
+              <a href='https://abbyeatslocal.com'>Abby Eats Local</a>
+            </li>
+            <li>
+              <a href='https://okforok.com'>Okay for Oklahoma</a>
+            </li>
+            <li>
+              <a href='https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html?&utm_campaign=gc-hc-sc-coronaviruspublicedu2021-2021-0001-9762248618&utm_medium=search&utm_source=google-ads-99837326356&utm_content=text-en-428935858540&utm_term=coronavirus'>
+                COVID-19 in Canada
+              </a>
+            </li>
           </ul>
         </div>
         <div className='footer_content-column'>
-          <h4>Contributions</h4>
+          <h4>Contributors</h4>
           <ul>
-            <li>Github</li>
+            <li>
+              <a href='https://twitter.com/mykalmachon'>Mykal Machon</a>
+            </li>
+            <li>
+              <a href='https://www.instagram.com/taylorkrulicki/?hl=en'>
+                Taylor Krulicki
+              </a>
+            </li>
+            <li>
+              <a href='https://github.com/MykalMachon/supportlocalfv.ca'>
+                Project's GitHub
+              </a>
+            </li>
           </ul>
         </div>
         <div className='footer_logo-column'>
