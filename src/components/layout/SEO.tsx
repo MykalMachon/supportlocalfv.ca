708d25f130b34e6f90a7f5c902302c0bef5970b2
Mykal Machon <mykalmachon@gmail.com>
2020-05-01 00:27:35 -0700

    Added a dynamic SVG favicon

--- src/components/layout/SEO.tsx
+++ src/components/layout/SEO.tsx
@@ -14,6 +14,10 @@ const SEO: FC<SEOProps> = ({ title }) => {
         name='description'
         content='Support Local Fraser Valley is a community driven directory of local businesses. Our main goal is to highlight exemplary local businesses who may need support due to the COVID-19 Pandemic.'
       />
+      <link
+        rel='icon'
+        href='https://res.cloudinary.com/drwst2p1d/image/upload/v1588316487/Support%20Local%20Fraser%20Valley/Logos/PNG/Bird%20Only/Bird_Dark_Dynamic.svg'
+      />
     </Head>
   );
 };
